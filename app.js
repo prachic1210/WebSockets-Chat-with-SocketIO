2a13507f9d821bca909cbb1a1f195f7332fc65ef
Prachi <prachic1210@gmail.com>
2022-04-03 01:32:19 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { ppid } = require('process');
+const { isObject } = require('util');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+let port = 3000;
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket)=> {
+    console.log('a user connected');
+    socket.on('chat message', (msg) =>{
+        io.emit('chat message', msg);
+        //console.log('message' + msg);
+    });
+    socket.on('disconnect', () =>{
+        console.log('user disconnected');
+    });
+});
+
+server.listen(3000, ()=>{
+    console.log('...listening on port ', port);
+});
